c2211da59f948b7fb0ca18f00e5122de76b825f6
Lucas <oliveira.lucas20@gmail.com>
2022-12-23 22:33:40 -0400

    add

--- app/src/main/java/com/lucassimao/helloci_cd/MainActivity.kt
+++ app/src/main/java/com/lucassimao/helloci_cd/MainActivity.kt
@@ -1,4 +1,4 @@
-git package com.lucassimao.helloci_cd
+import com.lucassimao.helloci_cd.R
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        print("Hello")
+
     }
 }
